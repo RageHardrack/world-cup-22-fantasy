da9f8e7d34fc854eb7ba48d52b00847cbc9458c1
Daniel <dacolmenares93@gmail.com>
2022-12-07 07:20:23 -0500

    Feat: Empezando UI Fantasy

--- pages/fantasy/index.vue
+++ pages/fantasy/index.vue
@@ -8,10 +8,57 @@ definePageMeta({
 </script>
 
 <template>
-  <section class="container">
-    <h1>Fantasy</h1>
-    <h2>Próximamente</h2>
+  <section class="flex flex-col items-center justify-center w-full">
+    <article
+      class="flex flex-col justify-center w-full max-w-6xl p-2 space-y-2 rounded bg-color-2"
+    >
+      <header class="w-full py-2 text-center rounded bg-color-4">
+        <h2 class="text-3xl">Formaciones</h2>
+      </header>
 
-    {{ data }}
+      <section class="flex items-center justify-center gap-x-2">
+        <picture class="flex-1">
+          <img
+            src="/img/campo_futbol.png"
+            atl="Campo de Fútbol"
+            class="w-full"
+          />
+        </picture>
+
+        <section class="flex flex-col items-center justify-between flex-1 w-full h-full">
+          <header class="flex flex-col w-full space-y-2">
+            <input
+              type="text"
+              class="bg-transparent text-color-4 placeholder:text-color-3"
+              placeholder="Mi Equipo"
+            />
+
+            <div class="flex">
+              <input
+                type="text"
+                class="bg-transparent text-color-4 placeholder:text-color-3"
+                placeholder="Buscar Jugador"
+              />
+              Buscar
+            </div>
+
+            <div class="flex w-full">
+              <ButtonTab>Porteros</ButtonTab>
+              <ButtonTab>Centrales</ButtonTab>
+              <ButtonTab>Mediocampo</ButtonTab>
+              <ButtonTab>Delanteros</ButtonTab>
+            </div>
+          </header>
+
+          <ul class="flex-1 h-full">
+            <li>hola</li>
+            <li>hola</li>
+            <li>hola</li>
+            <li>hola</li>
+            <li>hola</li>
+          </ul>
+        </section>
+      </section>
+    </article>
   </section>
 </template>
